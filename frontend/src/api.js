638b815516da342324322b464d6f46bc53355711
varunsanju7972 <sanjay.varun@iitg.ac.in>
2025-04-18 10:34:15 +0530

    api is updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// connect to your backend running at localhost:8000
 export const getPredictions = () => {
-  return axios.get("http://127.0.0.1:8000/predict/");
+  return axios.get("https://real-time-crypto-2.onrender.com/predict/");
 };
